9b5bf7abfb07181b46391dc700dae88a0e687417
Marcus Jun <mjun@calstate.edu>
2018-04-04 19:38:18 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,9 +15,10 @@
 def speak_to_grandma(string)
   if (string!=string.upcase)
     "HUH?! SPEAK UP, SONNY!"
-  elsif (string==string.upcase)
-    "NO, NOT SINCE 1938!"
   elsif (string=="I LOVE YOU GRANDMA!")
     "I LOVE YOU TOO PUMPKIN!"
+  elsif (string==string.upcase)
+    "NO, NOT SINCE 1938!"
+
   end
 end
